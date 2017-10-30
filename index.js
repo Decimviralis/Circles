af3098646cc1cee73c1491c4a9d8fc5ebf33405c
maxim <decimviralis@gmail.com>
2017-10-30 16:01:34 +0600

    fix

--- index.js
+++ index.js
@@ -1,43 +1,51 @@
-var width = innerWidth/4;
-
-var height = innerHeight/2;
-
+var width = window.innerWidth;
+var height = window.innerHeight;
 var app;
+var colors = [0xFFFF0B, 0xFF700B, 0x4286f4, 0x4286f4, 0xf441e8, 0x8dff6d, 0x41ccc9, 0xe03375, 0x95e032, 0x77c687, 0x43ba5b, 0x0ea3ba];
+var gravity = 4;
+var figuresAmount = -1;
+var figure = [];
 
-var j = 0;
 
-var colors = [0x66CDAA, 0xF08080, 0x6B8E23, 0xFFFF00, 0xFF6347];
 
 var model = {
-    figure : [],
-    gravity : 4,
-    figuresAmount : -1,
     createCanvas: function() {
         app = new PIXI.Application(width, height);
         document.body.appendChild(app.view);
     },
-
-    drawCircle: function () {
+    drawCircle: function() {
         rand = Math.floor(Math.random() * colors.length);
-        var radius = width/8;
-        var inAreaX = 3*width/4;
-        var circleY = 50;
-        var circleX;
-        for (var i = 0; i < width; i+=radius) {
-            circleX = i-width+86;
-        }
+        var radius = 50;
+        var inAreaX = width - 100;
+        var circleY = -50;
+        var circleX = Math.floor(Math.random() * inAreaX);
         var circle = new PIXI.Graphics();
-        circle.lineStyle(0); //начинаем рисовать
-        circle.beginFill(colors[rand], 1); //задаем рандомный цвет
-        circle.drawCircle(circleX, circleY, radius); //рисование круга
-        circle.endFill(); //закончили отрисовку
-
-        for (j = 0; j < width; j+=radius) {
-            app.stage.addChild(circle);
-        } ///выводим круг на холсте
+        circle.lineStyle(0);
+        circle.beginFill(colors[rand], 1);
+        circle.drawCircle(circleX, circleY, radius);
+        circle.endFill();
+        circle.interactive = true;
+        circle.buttonMode = true;
+        circle.live = true;
+        figuresAmount++;
+        circle.num = figuresAmount;
+        figure.push(circle);
+        app.stage.addChild(circle);
+        circle.on('pointerdown', controller.clearFigure);
+    },
+    gameOver: function() {
+        var style = new PIXI.TextStyle({
+            fill: '0xffffff',
+            fontSize: 36
+        });
+        var gameOverText = new PIXI.Text('Game Over', style);
+        gameOverText.x = width / 2;
+        gameOverText.y = height / 2;
+        gameOverText.pivot.x = 50;
+        gameOverText.pivot.y = 50;
+        app.stage.addChild(gameOverText);
     }
-};
-
+}
 var view = {
     loadGame: function() {
         model.createCanvas();
@@ -45,18 +53,26 @@ var view = {
 
         setInterval(model.drawCircle, 500);
 
-        app.ticker.add(function() { //постоянное обновление холста
-            for (var i = 0; i < model.figuresAmount; i++) {
-                figure[i].position.y += gravity; //заставляем гравитацию работать
+        app.ticker.add(function() {
+            for (var i = 0; i < figuresAmount; i++) {
+                figure[i].position.y += gravity;
+                if (figure[i].position.y > height && figure[i].live == true) {
+                    model.gameOver();
+                    return false;
+                }
+
             }
-    });
+        });
     }
-};
+}
+
 
 var controller = {
-    clearFigure: function () {
+    clearFigure: function() {
         this.clear();
+        figure[this.num].live = false;
+
     }
-};
+}
 
 view.loadGame();
